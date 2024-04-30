f780383b57753400d77b3bcee06bb081065fbc33
Daron Weekley <dweekley99@gmail.com>
2024-04-30 12:35:41 -0400

    changed link to pdf

--- My-Webpage/src/pages/Publications.jsx
+++ My-Webpage/src/pages/Publications.jsx
@@ -4,7 +4,7 @@ const Publications = () => {
   return (
     <div className='story-container'>
       <h1 className='underline'>Publications</h1>
-      <iframe src="./src/antibiotics-10-00992.pdf" width="800" height="600">
+      <iframe src="antibiotics-10-00992.pdf" width="800" height="600">
         Sorry, your browser does not support PDFs. <a href="./src/antibiotics-10-00992.pdf">Download PDF</a>
       </iframe>
     </div>
