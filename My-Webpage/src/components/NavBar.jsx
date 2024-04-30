82fac9f0eb1de7720d2cd7d008e58966bbb8616b
Daron Weekley <dweekley99@gmail.com>
2024-04-30 12:34:58 -0400

    moving pdf, adding '/' to links

--- My-Webpage/src/components/NavBar.jsx
+++ My-Webpage/src/components/NavBar.jsx
@@ -7,10 +7,10 @@ function NavBar() {
         <nav className="navbar">
             <ul className="nav-links">
                 <li><NavLink to="/Daron/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
-                <li><NavLink to="/my-story" className={({ isActive }) => isActive ? "active" : ""}>My Story</NavLink></li>
-                <li><NavLink to="/work-experience" className={({ isActive }) => isActive ? "active" : ""}>Work Experience</NavLink></li>
-                <li><NavLink to="/publications" className={({ isActive }) => isActive ? "active" : ""}>Publications</NavLink></li>
-                <li><NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact Information</NavLink></li>
+                <li><NavLink to="/my-story/" className={({ isActive }) => isActive ? "active" : ""}>My Story</NavLink></li>
+                <li><NavLink to="/work-experience/" className={({ isActive }) => isActive ? "active" : ""}>Work Experience</NavLink></li>
+                <li><NavLink to="/publications/" className={({ isActive }) => isActive ? "active" : ""}>Publications</NavLink></li>
+                <li><NavLink to="/contact/" className={({ isActive }) => isActive ? "active" : ""}>Contact Information</NavLink></li>
             </ul>
         </nav>
     );
