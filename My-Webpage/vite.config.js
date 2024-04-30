a9716549124c3f0693497831917690e751d4f9b0
Daron Weekley <dweekley99@gmail.com>
2024-04-30 11:55:04 -0400

    Preparing for website upload

--- My-Webpage/vite.config.js
+++ My-Webpage/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Daron/',
   plugins: [react()],
+  root: '.', 
+  build: {
+    outDir: 'dist', 
+  }
 })
