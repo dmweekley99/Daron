1ca1313eceec50bc08df0a62dda374e4a5be48f1
Daron Weekley <dweekley99@gmail.com>
2024-04-30 18:16:31 -0400

    changed base again to Daron

--- My-Webpage/vite.config.js
+++ My-Webpage/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Daron/',
   plugins: [react()],
   root: '.', 
   build: {
