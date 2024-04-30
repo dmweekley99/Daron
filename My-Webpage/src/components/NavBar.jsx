db0507fff271468b191401f32a50666d58178bcc
Daron Weekley <dweekley99@gmail.com>
2024-04-30 18:23:44 -0400

    Hopefully final change, changed path to /Daron/antibiotics

--- My-Webpage/src/components/NavBar.jsx
+++ My-Webpage/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <nav className="navbar">
             <ul className="nav-links">
-                <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
+                <li><NavLink to="/Daron/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
                 <li><NavLink to="/my-story/" className={({ isActive }) => isActive ? "active" : ""}>My Story</NavLink></li>
                 <li><NavLink to="/work-experience/" className={({ isActive }) => isActive ? "active" : ""}>Work Experience</NavLink></li>
                 <li><NavLink to="/publications/" className={({ isActive }) => isActive ? "active" : ""}>Publications</NavLink></li>
--- My-Webpage/src/pages/Publications.jsx
+++ My-Webpage/src/pages/Publications.jsx
@@ -4,8 +4,8 @@ const Publications = () => {
   return (
     <div className='story-container'>
       <h1 className='underline'>Publications</h1>
-      <iframe src="/antibiotics-10-00992.pdf" width="800" height="600">
-        Sorry, your browser does not support PDFs. <a href="/antibiotics-10-00992.pdf">Download PDF</a>
+      <iframe src="/Daron/antibiotics-10-00992.pdf" width="800" height="600">
+        Sorry, your browser does not support PDFs. <a href="/Daron/antibiotics-10-00992.pdf">Download PDF</a>
       </iframe>
     </div>
   ) 
