9683d97cdfd516d60a00f506134ff355a87f9a5f
Daron Weekley <dweekley99@gmail.com>
2024-06-06 13:40:48 -0400

    Configure github deployment

--- My-Webpage/src/components/NavBar.jsx
+++ My-Webpage/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <nav className="navbar">
             <ul className="nav-links">
-                <li><NavLink exact to="/Daron/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
                 <li><NavLink to="/my-story/" activeClassName="active">My Story</NavLink></li>
                 <li><NavLink to="/work-experience/" activeClassName="active">Work Experience</NavLink></li>
                 <li><NavLink to="/publications/" activeClassName="active">Publications</NavLink></li>
