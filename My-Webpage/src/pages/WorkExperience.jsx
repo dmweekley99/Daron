8389c97bf5272defebdb31c3c991044d16c0326c
Daron Weekley <dweekley99@gmail.com>
2026-04-23 12:55:26 -0400

    Trying to fix hostinger only displaying update on refresh

--- My-Webpage/src/pages/WorkExperience.jsx
+++ My-Webpage/src/pages/WorkExperience.jsx
@@ -16,9 +16,9 @@ const WorkExperience = () => {
         <h3>
           West Liberty University | 12/25 - Present
         </h3>
-            <p>Performed a variety of cell and bacterial cultures to run genetic testing and RNA sequencing</p>
-            <p>Launched statistical tests to make graphics to present the results of research.</p>
-            <p>Collaborated with professors and students in a professional setting.</p>
+        <p>Performed a variety of cell and bacterial cultures to run genetic testing and RNA sequencing</p>
+        <p>Launched statistical tests to make graphics to present the results of research.</p>
+        <p>Collaborated with professors and students in a professional setting.</p>
       </div>
       <div className='work-section'>
         <h2 className='underline'>
@@ -27,9 +27,9 @@ const WorkExperience = () => {
         <h3>
           Next Gen Federal Systems | 1/25 - 1/26
         </h3>
-            <p>Develop and deploy Angular-based frontends for ML pipelines</p>
-            <p>Use Docker, Kubernetes, Helm, Git, and WSL for container orchestration</p>
-            <p>Enhance usability and customer satisfaction in a machine learning platform</p>
+        <p>Develop and deploy Angular-based frontends for ML pipelines</p>
+        <p>Use Docker, Kubernetes, Helm, Git, and WSL for container orchestration</p>
+        <p>Enhance usability and customer satisfaction in a machine learning platform</p>
       </div>
       <div className='work-section'>
         <h2 className='underline'>
@@ -38,31 +38,31 @@ const WorkExperience = () => {
         <h3>
           Marshall University | 11/24 - 06/25
         </h3>
-            <p>Used PyTorch and vedo to analyze cranial landmarks in 3D mesh data</p>
-            <p>Worked on decimation and segmentation tradeoffs in oral-cranial structures</p>
-            <p>Published findings on efficiency and segmentation accuracy balance</p>
+        <p>Used PyTorch and vedo to analyze cranial landmarks in 3D mesh data</p>
+        <p>Worked on decimation and segmentation tradeoffs in oral-cranial structures</p>
+        <p>Published findings on efficiency and segmentation accuracy balance</p>
       </div>
       <div className='work-section'>
         <h2 className='underline'>
-          Substitute Teacher 
+          Substitute Teacher
         </h2>
         <h3>
           Tyler County Schools in West Virginia | 8/23 - Present
         </h3>
-            <p>Managed classrooms and followed lesson plans across grade levels</p>
-            <p>Strengthened leadership, communication, and public speaking</p>
+        <p>Managed classrooms and followed lesson plans across grade levels</p>
+        <p>Strengthened leadership, communication, and public speaking</p>
       </div>
       <div className='work-section'>
         <h2 className='underline'>
-          Ph.D. Student Researcher (Biomedical Science)    
+          Ph.D. Student Researcher (Biomedical Science)
         </h2>
         <h3>
-          Marshall University | 7/22 - 7/23        
+          Marshall University | 7/22 - 7/23
         </h3>
-          <p>Developed murine-human gene database using R</p>
-          <p>Focused on genetic sequencing, bioinformatics workflows</p>
-          <p>Worked with cell cultures, EM imaging, and murine harvesting</p>
-          <p>Co-author to published findings on the impact of murine NAS on astrocyte function.</p>
+        <p>Developed murine-human gene database using R</p>
+        <p>Focused on genetic sequencing, bioinformatics workflows</p>
+        <p>Worked with cell cultures, EM imaging, and murine harvesting</p>
+        <p>Co-author to published findings on the impact of murine NAS on astrocyte function.</p>
       </div>
       <div className='work-section'>
         <h2 className='underline'>
@@ -71,18 +71,18 @@ const WorkExperience = () => {
         <h3>
           West Liberty University | 5/21 - 5/22
         </h3>
-          <p>Worked on histology, dissection, lab management, sanitation protocols, statistical analysis</p>
-          <p>Integrated technology to improve lab workflows</p>
+        <p>Worked on histology, dissection, lab management, sanitation protocols, statistical analysis</p>
+        <p>Integrated technology to improve lab workflows</p>
       </div>
       <div className='work-section'>
         <h2 className='underline'>
           Undergraduate Researcher (Biology)
         </h2>
         <h3>
-          West Liberty University | 5/19 - 3/20           
+          West Liberty University | 5/19 - 3/20
         </h3>
-          <p>Developed skills using PCR, purification, sterilization, and lab management</p>
-          <p>Co-author to published findings on the genetic susceptibility of bacteria to a novel antibiotic.</p>
+        <p>Developed skills using PCR, purification, sterilization, and lab management</p>
+        <p>Co-author to published findings on the genetic susceptibility of bacteria to a novel antibiotic.</p>
       </div>
     </div>
   )
